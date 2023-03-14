5fc5b035e0030bc2cf620249340f162d617ab6f1
Tara John <tarathankachan91@gmail.com>
2023-03-14 22:18:49 +0100

    area perimiter of circle

---
+++ Area_peri_circle.py
@@ -0,0 +1,5 @@
+import math
+r=int(input('Enter radius of circle:\n'))
+area= math.pi*r*r
+perimeter=2*math.pi*r
+print(f'The area of the circle is {area:.2f} and The perimeter of the circle is {perimeter:.2f}')
